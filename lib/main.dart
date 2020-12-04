e9c74137fbe112b4ea8b60ef0f1af5fcbe750aa8
Haris Putratama <lastblezebub@gmail.com>
2020-12-04 20:42:14 +0700

    Adding fonts + image

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         ),
         textTheme: globalTextTheme,
       ),
-      home: Home(),
+      home: Login(),
     );
   }
 }
--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -3,6 +3,29 @@ import 'package:flutter/material.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+      return MaterialApp(
+        title: 'Welcome to Flutter',
+        home: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/image/background_login.png"), fit: BoxFit.cover)),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              title: Text('My App'),
+              centerTitle: true,
+              leading: IconButton(
+                  icon: Icon(
+                    Icons.list,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {}),
+            ),
+          ),
+        ),
+      );
+    }
   }
-}
+
